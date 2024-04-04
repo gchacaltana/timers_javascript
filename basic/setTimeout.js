b52ed5140c9d1c00dcfb2d3513707e4f5c35ce0a
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2024-04-03 22:33:50 -0500

    update script setTimeout

--- basic/setTimeout.js
+++ basic/setTimeout.js
@@ -8,7 +8,7 @@
 function main(args) {
 
     // Elimina los dos primeros argumentos que son el comando y el nombre del script
-    args = args.slice(2);
+    const args = args.slice(2);
 
     // Si no se pasan argumentos, se muestra un mensaje y se termina la ejecución
     if (args.length === 0) {
@@ -17,7 +17,7 @@ function main(args) {
     }
 
     // Se obtiene el primer argumento y se convierte a entero
-    seconds = parseInt(args[0]);
+    const seconds = parseInt(args[0]);
 
     // Si el argumento no es un número, se muestra un mensaje y se termina la ejecución
     if (isNaN(seconds)) {
